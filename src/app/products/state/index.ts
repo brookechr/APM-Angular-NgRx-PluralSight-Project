cae456f0e8742a67618f26eb5e50395815bb220a
Brooke Christiansen <bchristiansen@ceiamerica.com>
2020-05-27 16:14:51 -0400

    first commit

---
+++ src/app/products/state/index.ts
@@ -0,0 +1,52 @@
+/* NgRx */
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import * as fromRoot from '../../state/app.state';
+import * as fromProducts from './product.reducer';
+
+// Extends the app state to include the product feature.
+// This is required because products are lazy loaded.
+// So the reference to ProductState cannot be added to app.state.ts directly.
+export interface State extends fromRoot.State {
+  products: fromProducts.ProductState;
+}
+
+// Selector functions
+const getProductFeatureState = createFeatureSelector<fromProducts.ProductState>('products');
+
+export const getShowProductCode = createSelector(
+  getProductFeatureState,
+  state => state.showProductCode
+);
+
+export const getCurrentProductId = createSelector(
+  getProductFeatureState,
+  state => state.currentProductId
+);
+
+export const getCurrentProduct = createSelector(
+  getProductFeatureState,
+  getCurrentProductId,
+  (state, currentProductId) => {
+    if (currentProductId === 0) { //new product
+      return {
+        id: 0,
+        productName: '',
+        productCode: 'New',
+        description: '',
+        starRating: 0
+      };
+    } else {
+      return currentProductId ? state.products.find(p => p.id === currentProductId) : null; //find() is an array method that finds that array element whose id matches the currentProductId; if none found, null
+    }
+  }
+);
+
+export const getProducts = createSelector(
+  getProductFeatureState,
+  state => state.products
+);
+
+export const getError = createSelector(
+  getProductFeatureState,
+  state => state.error
+)
